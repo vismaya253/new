05f8d5089c478c8005dc230698f049bed824190c
vismaya253 <vismayamk@gmail.com>
2023-12-04 18:38:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+//
+// /{
+// // // //     var p="hello world";
+    
+
+// }
+//  console.log(p);
+// const arr=[10,20,30,40];
+//console.log(arr);
+// console.log(arr[2]);
+
+// for (let i=0;i<5;i++){
+//     console.log(i);
+//  }
+// const arr=[10,20,35];
+// console.log(arr.length); 
+// let arr=[10,20,45,50];
+// arr.push(100)
+// console.log(arr.length);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// const a=arr.indexOf(45)
+// console.log(arr.indexOf(80));
+
+// const data=["aws","google","microsoft"];
+// console.log(...data);
+// const data=["aws","google","microsoft"];
+// const arr=["surabhi","vijayashri"];
+// for(let i=0;i<data.length;i++){
+//     arr.push(data[i]);
+//     arr.pop();
+// }
+// const obj={
+//     name:"surabhi",
+//     usn:"4al22is060",
+//     hobbies:["birding","swimming"],
+//     a:1
+// }
+// const {name,hobbies}=obj;
+// console.log(name,hobbies);
+// const arr={p:20};
+// console.log(typeof arr);
+// const val="hi";
+// console.log(typeof val);
+// const val=30;
+// console.log(typeof val);
+// const val=true;
+// console.log(typeof val);
+
+// const obj={
+//     userName:
+// };
+// const keys=["name","usn","key"]
+// for(let i=0;i<keys.length;i++){
+
+
+// console.log(obj[keys[i]]);
+// }
+//  const account={
+//     userName:"test",
+//     password:"test@123",
+//     details:{email:["test@gmail.com","test1@gmail.com"],phno:123456789}
+//  }
+// const [primaryEmail,recoveryEmail]=account.details.email;
+// console.log(recoveryEmail)
+// const emails=account.details.email;
+// const val=[10,20,30];
+// console.log(...val);
+// const googleDatabase=[{gmail:"test@gmail.com",owner:"test",password:"1234"},{gmail:"siri@gmail",owner:"siri",password:"5678"}];
+// const len=googleDatabase.length;
+// for(let i=0;i<len;i++)
+// console.log(googleDatabase[i].gmail)
+
+// const a=[15,35,40];
+// const b=20;
+// const str=`the number is ${a} and ${b}`;
+// console.log(str);
+
+const obj={
+    name:"test",
+}
+console.error("hello");
+
+
+
+
+
+
+
+
+
+ 
